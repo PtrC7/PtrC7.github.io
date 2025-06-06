f462e063e47354830c5eeb9b09d976ccdf34a802
PtrC7 <petechaya7@gmail.com>
2025-06-06 16:31:21 -0400

    Optimized navbar and fixed projects on home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+// navbar
+function loadNavbar() {
+  fetch('navbar.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('navbar').innerHTML = data;
+      });
+}
+
+window.onload = loadNavbar;
+
 // Nav hamburgerburger selections
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
